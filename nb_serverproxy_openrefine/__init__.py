753eb9546ec3798a77cb1f0d8b07084b022de342
Tony Hirst <tony.hirst@gmail.com>
2021-06-23 23:31:56 +0100

    __init__.py improce default openrefine dir handling

--- nb_serverproxy_openrefine/__init__.py
+++ nb_serverproxy_openrefine/__init__.py
@@ -1,10 +1,11 @@
-__version__="0.0.1"
+__version__="0.0.2"
 
 import os
+from pathlib import Path
 
 # Requires openrefine to be on the path as: refine
 def setup_openrefine():
-    openrefine_path = '~/openrefine'
+    openrefine_path =  os.getenv("REFINE_DIR") if "REFINE_DIR" in os.environ else str(Path.home() / 'openrefine')
     if not os.path.exists(openrefine_path):
         os.makedirs(openrefine_path)
     return {
